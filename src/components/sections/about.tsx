fe966f5d2bd598c7a949a7bc30aa47236d933e2c
angeluciel <jpmizidoro@gmail.com>
2026-06-12 20:48:18 +0000

    component creation

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -1,3 +1,28 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger, useGSAP);
+
 export function About() {
-  return <div></div>
+  useGSAP(() => {
+    gsap.to('#box', {
+      xPercent: 90,
+      scrollTrigger: {
+        trigger: '#box',
+        start: 'top top',
+        end: '+=2000',
+        markers: true,
+        scrub: true,
+      },
+    });
+  }, []);
+
+  return (
+    <section className='relative h-500 bg-zinc-900 w-full py-12 px-6'>
+      <div className='sticky top-0 p-4 rounded-lg' id='box'>
+        <div className='bg-zinc-800 w-fit p-4 rounded-lg'>alo</div>
+      </div>
+    </section>
+  );
 }
